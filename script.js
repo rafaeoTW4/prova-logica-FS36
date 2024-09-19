afa28ede55c6b5e7ed9c0ebc5c89958a32dcdb92
RAFAEL MARCAL PARENTE <RAFAEL0545@DIGITALCOLLEGE.LOCAL>
2024-09-19 20:13:11 -0300

    quarto-commit

---
+++ script.js
@@ -0,0 +1,20 @@
+import usuarios from './login';
+
+        
+        document.getElementById('loginForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            
+            const email = document.getElementById('email').value;
+            const senha = document.getElementById('senha').value;
+
+            
+            const usuarioEncontrado = usuariosCadastrados.find(usuario => usuario.email === email && usuario.senha === senha);
+
+            if (usuarioEncontrado) {
+            
+                window.location.href = 'home.html'; // adicionar página html home
+            } else {
+                alert('Email ou senha inválidos!');
+            }
+        });
